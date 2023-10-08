624c47555f3a3ceabeee6ddf434a63fbae2b2bce
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-10-08 07:09:35 +0000

    [Add] 仮マイページ

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,8 +1,23 @@
 class Public::UsersController < ApplicationController
   def show
+    @user = current_user
+    @coordinates = current_user.coordinates
+    @bookmarks = current_user.bookmarks
   end
 
   def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      flash[:notice] = "会員情報を変更しました。"
+      redirect_to public_user_path
+    else
+      flash[:notice] = "変更内容が正しくありません。"
+      redirect_to public_customer_edit_path
+    end
   end
 
   def confirm
