9d05118d3272fc444253a356b586f807e0411c10
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-10-09 09:51:41 +0000

    [Add] タグgemと画像プレビュー機能

--- app/controllers/public/coordinates_controller.rb
+++ app/controllers/public/coordinates_controller.rb
@@ -1,13 +1,22 @@
 class Public::CoordinatesController < ApplicationController
   def new
+    @coordinate = Coordinate.new
   end
 
   def index
   end
 
   def show
+    @coordinate = Coordinate.find(params[:id])
+    @tags = @coordinate.tag_counts_on(:tags)
   end
 
   def edit
+  end
+
+  private
+
+  def coordinate_params
+    params.require(:coordinate).permit(:coordinate_image, :style, :introduction, :tag_list)
   end
 end
