c6a9e631120d9c455f93ba3c85d3caddb797b45b
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-10-14 04:12:56 +0000

    [Add]　管理者側会員機能

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,19 @@
 class Admin::UsersController < ApplicationController
-  def index
+  def show
+    @user = User.find(params[:id])
+    @coordinates = @user.coordinates
   end
 
-  def show
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    flash[:notice] = "会員情報を変更しました。"
+    redirect_to request.referer
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:is_deleted)
   end
 end
