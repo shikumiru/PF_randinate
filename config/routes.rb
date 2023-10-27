54ea2fdd16e9e720904d90d7997bec244af9876f
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-10-27 06:10:06 +0000

    [Fix] ルートパス再修正

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   end
 
   scope module: :public do
-    root to: "public/homes#top"
+    root to: "homes#top"
     resources :users, only: [:show, :edit, :update] do
       member do
         patch :deactivate
